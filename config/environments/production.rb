be315216778735eb9576ae4eaa81f4c3012801c5
pennymark <33641617+pennymark@users.noreply.github.com>
2021-07-02 20:17:18 -0400

    Update Credentials - Corrected Production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options ={:host => 'pennymark-photo-app.herokuapp.com', :protocol => 'https'
+  config.action_mailer.default_url_options ={:host => 'pennymark-photo-app.herokuapp.com', :protocol => 'https'}
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
